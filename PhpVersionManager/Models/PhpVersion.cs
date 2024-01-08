84fd8c8e443318684ad24666e19ec251f8dd6b70
Ricardo Boss <mail@ricardoboss.de>
2024-01-08 04:13:59 +0100

    Add hint

--- PhpVersionManager/Models/PhpVersion.cs
+++ PhpVersionManager/Models/PhpVersion.cs
@@ -2,5 +2,9 @@
 
 public record PhpVersion(int Major, int Minor, int Patch)
 {
+    /// <summary>
+    /// This must always result in a valid filename.
+    /// </summary>
+    /// <returns>The version as a string.</returns>
     public override string ToString() => $"{Major}.{Minor}.{Patch}";
 }
